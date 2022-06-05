31009ae26b7aee61413a1f5d5e5fd4f476e245a0
ClearCrystalSkies <100354673+ClearCrystalSkies@users.noreply.github.com>
2022-06-05 10:06:44 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+function allNew(){
+    window.location = "allNew.html";
+}
+function Super(){
+    window.location = "super.html";
+}
+function triology(){
+    window.location= "triology.html";
+}
+function mystery(){
+    window.location = "mystery.html";
+}
